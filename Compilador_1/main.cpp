67c8e4dc60db2edd43e336a0b99922e0f7dd8856
LuisilloElProgramador <56002200+LuisilloElProgramador@users.noreply.github.com>
2021-11-06 13:36:06 -0700

    Add files via upload

---
+++ Compilador_1/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <fstream>
+#include "Buffer.h"
+
+using namespace std;
+
+//******************************************************************************************
+/** \brief Lee los caracteres de un archivo*
+* \param entrada El archivo a leer
+*
+*/void Lectura(ifstream &entrada);
+
+//******************************************************************************************
+
+
+int main()
+{
+    std::ifstream entrada;
+
+    entrada.open("stream.txt");
+    if(!entrada)
+    {
+        std::cout<<"Catastrofe";
+        entrada.close();
+        return -1;
+    }
+    Lectura(entrada);
+
+
+    entrada.close();
+    return 0;
+}
+
+
+//******************************************************************************************
+void Lectura(ifstream &entrada )
+{
+    char aux;
+    Buffer bufAL(300);
+
+    if(entrada.get(aux)) bufAL.Meter(aux);
+    else return;
+
+    while(entrada.get(aux))
+    {
+        if(bufAL.EstaLleno())
+        {
+            cout<<bufAL[0];
+            //esperate tantito al analizador sintactico
+            bufAL.Sacar(bufAL.CantidadCaracteres())
+
+        }
+        bufAL.Meter(aux)
+    }
+    cout<<bufAL;
+}
