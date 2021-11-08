bd08993615ec7e8559b2038973d6df8a759eb8c1
LuisilloElProgramador <56002200+LuisilloElProgramador@users.noreply.github.com>
2021-11-07 17:12:11 -0700

    Add files via upload

---
+++ AnalizadorSintactic/ASintactico.cpp
@@ -0,0 +1,269 @@
+/* Nombre: .cpp
+   Autor:
+   Fecha:
+   Descripción:
+*/
+
+#include "ASintactico.h"
+
+
+
+ /*****************************************************************************************
+ **********************************   Constructores    ************************************
+ *****************************************************************************************/
+
+//*************************************************************************************
+ASintactico::ASintactico(const char *direccion)
+{
+    estActl=0;
+    std::ifstream archivo(direccion);
+    char transiciones[256];
+    ArbolBA<Llave> mapa;
+    std::string tGen;
+    char texto[400];
+    Llave aux,abusc;
+    int i;
+
+    if(!archivo)
+    {
+        throw DireccionNula();
+        archivo.close();
+        return;
+    }
+
+
+    //Leemos la primera linea para saber que caracter tine cada columna
+    archivo.getline(texto,400);
+    for(i=0; texto[i] != '\t' ; ++i);
+    for(int col=0; texto[i] != '\0' ; ++i)
+    {
+        if(texto[i] == '\t') continue;
+        transiciones[col++]= texto[i];
+    }
+
+
+    //Le asignamos una fila a cada estado
+    int valAux;
+    std::string sAux;
+    for(nEstados=0; !archivo.eof() ; ++nEstados)
+    {
+        archivo.getline(texto,400);
+
+        valAux=nEstados;
+        sAux.clear();
+        for(int j=0;texto[j]!='\t' && texto[j]!='*';++j)
+            sAux+=texto[j];
+
+        aux.Set(sAux,valAux);
+        mapa.Agregar(aux);
+    }
+
+
+
+    //Iniciamos los arreglos
+    eAcept =new bool[nEstados];
+    listTonken = new std::string[nEstados];
+    MatTrans= new int*[nEstados];
+    for(int i=0;i<nEstados;++i)
+        MatTrans[i] = new int[257];
+    for(int i=0;i<nEstados; ++i)
+        for(int j=0;j<257;++j)
+            MatTrans[i][j]=-1;
+
+
+    //llenamos la tabla, los estados de aceptacion y los T generados
+    int eTrans; //Variable aux, guardaa el valor que va en la pos [i][j]
+                    // (Ese valor se interpreta como "Estando en el estado 'i' y usando 'j' vete a [i][j]")
+    archivo.seekg(0, archivo.beg);
+    archivo.getline(texto,400);
+    for(int fila=0; !archivo.eof() ; ++fila)
+    {
+        archivo.getline(texto,400);
+
+        //Se lee el nombre del estado y se verifica si es de aceptacion o no
+        for(i=0;texto[i]!='\t' && texto[i] != '*' ;++i) ;
+        if(texto[i] == '*')
+        {
+            eAcept[fila] = true;
+            tGen.clear();
+            for(++i;texto[i]!='\t';++i)
+                tGen+=texto[i];
+            listTonken[fila] =tGen;
+        }
+        else eAcept[fila] = false;
+
+        //Se llena la matriz de transicion
+        for(int colD=0,colR;texto[i]!='\0';)
+        {
+
+            if(texto[i] == '\t')
+            {
+                ++i;
+                continue;
+            }
+
+            sAux.clear();
+            while(texto[i]!='\t' && texto[i] != '\0')
+                sAux+=texto[i++];
+            abusc.Set(sAux,8);
+            aux = mapa.Buscar(abusc);
+
+            eTrans = aux.GetValor();
+            colR=(int)transiciones[colD++];
+            MatTrans[fila][colR]=eTrans;
+
+            if(texto[i] != '\0') ++i;
+        }
+    }
+
+}
+
+
+//*************************************************************************************
+ASintactico::~ASintactico()
+{
+    delete [] eAcept;
+    delete [] listTonken;
+
+    for(int i=0;i<nEstados;++i)
+        delete [] MatTrans;
+    delete [] MatTrans;
+}
+
+//*************************************************************************************
+ASintactico::ASintactico(const ASintactico & a2)
+{
+    (*this) = a2;
+}
+
+ /*****************************************************************************************
+ ************************************    Operadores    ************************************
+ *****************************************************************************************/
+//*************************************************************************************
+ASintactico & ASintactico::operator= (const ASintactico & a2)
+{
+    return *this;
+}
+
+
+ /*****************************************************************************************
+ ********************************   Metodos y funciónes   *********************************
+ *****************************************************************************************/
+//*************************************************************************************
+void ASintactico::Reset()
+{
+    estActl=0;
+}
+
+//*************************************************************************************
+void ASintactico::Avanzar(const char c)
+{
+    if(MatTrans[estActl][(int)c] == -1) throw CaracterFuera();
+    estActl = MatTrans[estActl][(int)c];
+}
+
+//*************************************************************************************
+bool ASintactico::estadoAceptacion() const
+{
+    return eAcept[estActl];
+}
+
+
+//*************************************************************************************
+std::string ASintactico::GetT() const
+{
+    if(!estadoAceptacion())throw Grosero();
+    return listTonken[estActl];
+}
+
+ /*****************************************************************************************
+ ************************************   Excepciónes   *************************************
+ *****************************************************************************************/
+//*************************************************************************************
+const char * ASintactico::DireccionNula::what() const throw()
+{
+    return "No se encontr\242 la el archivo...";
+}
+
+//*************************************************************************************
+const char * ASintactico::Grosero::what() const throw()
+{
+    return "Hay una funcion para saber si el estado es de aaceptacion y no la usaste... Grosero...";
+}
+
+//*************************************************************************************
+const char * ASintactico::CaracterFuera::what() const throw()
+{
+    return "Se introdujo un caracter fuera del alfabeto...";
+}
+
+//*************************************************************************************
+const char * ASintactico::TablaIncorrecta::what() const throw()
+{
+    return "Hay un error en el automata del archivo...";
+}
+
+ /*****************************************************************************************
+ *********************************   Metodos privados   ***********************************
+ *****************************************************************************************/
+
+ /*****************************************************************************************
+ ***************************   Funciones externas y amigas   ******************************
+ *****************************************************************************************/
+ Llave::Llave()
+{
+    this->llave= "";
+    this->valor = -6;
+}
+
+
+void Llave::Set(const std::string & s, const int &v)
+{
+    llave=s;
+    valor=v;
+}
+
+int Llave::GetValor() const
+{
+    return valor;
+}
+
+Llave & Llave::operator= (const Llave & ll2)
+{
+    if(this == &ll2) return *this;
+
+    this->llave = ll2.llave;
+    this->valor = ll2.valor;
+
+    return *this;
+}
+
+Llave::Llave(const Llave & ll2)
+{
+    (*this) = ll2;
+}
+
+bool Llave::operator<(const Llave & ll2) const
+{
+    return this->llave < ll2.llave;
+}
+
+bool Llave::operator>(const Llave & ll2) const
+{
+    return this->llave > ll2.llave;
+}
+
+bool Llave::operator==(const Llave & ll2) const
+{
+    return this->llave == ll2.llave;
+}
+
+bool Llave::operator>=(const Llave & ll2) const
+{
+    return this->llave >= ll2.llave;
+}
+
+bool Llave::operator<=(const Llave & ll2) const
+{
+    return this->llave <= ll2.llave;
+}
---
+++ AnalizadorSintactic/ASintactico.h
@@ -0,0 +1,211 @@
+#ifndef ASINTACTICO_H_INCLUDED
+#define ASINTACTICO_H_INCLUDED
+
+/* Nombre: .h
+   Autor:
+   Fecha:
+   Descripción:
+*/
+
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <limits>
+#include <fstream>
+#include "ArbolBA.h"
+
+
+
+class ASintactico
+{
+
+ /*****************************************************************************************
+ ***************************   Funciones externas y amigas   ******************************
+ *****************************************************************************************/
+
+
+    //*************************************************************************************
+    /** \brief imprime una ASintactico en un flujo de salida
+    *
+    * \param salida el flujo de salida
+    * \return El mismo flujo de salida
+    *
+    */friend std::ostream & operator<<(std::ostream & salida,const ASintactico &x);
+
+    //*************************************************************************************
+    /** \brief
+     *
+     * \param entrada el flujo de entrada
+     * \return El mismo flujo de entrada
+     *
+     */friend std::istream & operator>>(std::istream & entrada, ASintactico &x);
+
+public:
+
+ /*****************************************************************************************
+ **********************************   Constructores    ************************************
+ *****************************************************************************************/
+
+    //*************************************************************************************
+    /** \brief Constructor que "abre" la matriz en el documento 'dir'
+    *
+    *   \param dir La direccion del blog de notas dondé se guarda la matrizDireccionNula
+    *   \exception std::bad_alloc::matrizDireccionNula No se encontro el blog
+    *   \exception std::bad_alloc No hay memoria para las variables dinamicas
+    *
+    *
+    */ASintactico(const char  *dir);
+
+
+    //*************************************************************************************
+    /** \brief Constructor de copias
+    *   \param a2 El objeto a copiar
+    */ASintactico(const ASintactico & a2);
+
+
+    //*************************************************************************************
+    /** \brief Destructor de la clase
+    *
+    */~ASintactico();
+
+
+
+ /*****************************************************************************************
+ ************************************    Operadores    ************************************
+ *****************************************************************************************/
+
+
+    //*************************************************************************************
+    /** \brief Sobrecarga del operador '='
+    *
+    */ASintactico & operator= (const ASintactico & mtz);
+
+
+
+ /*****************************************************************************************
+ ********************************   Metodos y funciónes   *********************************
+ *****************************************************************************************/
+
+    //*************************************************************************************
+    /** \brief Pone el estado en el estado inicial (El primero)
+    *
+    */void Reset();
+
+    //*************************************************************************************
+    /** \brief Mueve el automata usando la transicion 'c'
+    *   \param c El caracter que se usara para la transicion
+    *   \exception std::bad_alloc::CaracterFuera 'c' está fuera del alfabeto
+    *
+    */void Avanzar(const char c);
+
+    //*************************************************************************************
+    /** \brief Mueve el automata usando la transicion 'c'
+    *   \return True si el estado actual es de aceptacion, false de lo contrario
+    *
+    */bool estadoAceptacion() const;
+
+    //*************************************************************************************
+    /** \brief Retorna el token del estado
+    *   \return True si el estado actual es de aceptacion, false de lo contrario
+    *   \exception std::bad_alloc::Grosero El estado actual no es de aceptacion
+    *
+    */std::string GetT() const;
+
+ /*****************************************************************************************
+ ************************************   Excepciónes   *************************************
+ *****************************************************************************************/
+
+    //*************************************************************************************
+    class CaracterFuera: public std::exception
+    {
+        public:
+            virtual const char * what() const throw();
+        private:
+
+    };
+
+    //*************************************************************************************
+    class DireccionNula: public std::exception
+    {
+        public:
+            virtual const char * what() const throw();
+        private:
+
+    };
+
+    //*************************************************************************************
+    class Grosero: public std::exception
+    {
+        public:
+            virtual const char * what() const throw();
+        private:
+
+    };
+
+    //*************************************************************************************
+    class TablaIncorrecta: public std::exception
+    {
+        public:
+            virtual const char * what() const throw();
+        private:
+
+    };
+
+
+private:
+    int nEstados;
+    int estActl;
+    int **MatTrans;
+    bool *eAcept;
+    std::string *listTonken;
+
+ /*****************************************************************************************
+ *********************************   Metodos privados   ***********************************
+ *****************************************************************************************/
+};
+
+class Llave
+{
+//*************************************************************************************
+    /** \brief imprime una ASintactico en un flujo de salida
+    *
+    * \param salida el flujo de salida
+    * \return El mismo flujo de salida
+    *
+    */friend std::ostream & operator<<(std::ostream & salida,const Llave &x)
+    {
+        salida<<'['<<x.llave<<" , "<<x.valor<<']';
+        return salida;
+    };
+
+public:
+
+    Llave();
+
+    Llave(const Llave & ll2);
+
+    void Set(const std::string & s, const int &v);
+
+    int GetValor() const;
+
+    Llave & operator= (const Llave & ll2);
+
+    bool operator<(const Llave & ll2) const;
+
+    bool operator>(const Llave & ll2) const;
+
+    bool operator==(const Llave & ll2) const;
+
+    bool operator>=(const Llave & ll2) const;
+
+    bool operator<=(const Llave & ll2) const;
+
+private:
+
+    std::string llave;
+    int valor;
+
+};
+
+
+#endif // ASINTACTICO_H_INCLUDED
